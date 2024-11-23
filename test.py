4e5c601d2a5347bbb199f0ea74023ba86c91ff6c
bolshakov863 <ivan.bolshakov113@gmail.com>
2024-11-23 15:41:13 +0300

    add new words in README

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from tkinter import*
+from tkinter import *
 
 # область переменных
 w = 800
@@ -10,12 +10,13 @@ x_finish = w - 50
 
 # область функций
 def key_handler(event):
-    canvas.move(player1_id, 10, 0)
+    canvas.move(player1_id, 10,0)
 
 # область создания окна и холста
 window = Tk()
 window.title('Догони меня если сможешь')
 canvas = Canvas(window, width=w, height=h, bg='white')
+canvas.pack()
 
 #область объектов
 player1_id = canvas.create_rectangle(x1,y1, x1 + player_size,y1 + player_size, fill = player1_color)
